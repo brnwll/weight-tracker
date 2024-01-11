fe476e017811cce021d6d82f855cf1621c654adf
Brian Howell <brianhowell@pm.me>
2024-01-10 21:47:17 -0500

    Testing: dataPoint mouseover changes state and updates the chart

--- src/App.js
+++ src/App.js
@@ -1,51 +1,68 @@
-import "./App.css";
+import React, { useState } from "react";
 import CanvasJSReact from "@canvasjs/react-charts";
+import "./App.css";
 
-const options = {
-  animationEnabled: true,
-  title: {
-    text: "Weight Tracker",
-  },
-  axisX: {
-    valueFormatString: "M/D",
-  },
-  axisY: {
-    title: "Pounds",
-    prefix: "",
-  },
-  data: [
-    {
-      yValueFormatString: "###.##",
-      xValueFormatString: "M/D",
-      type: "spline",
-      dataPoints: [
-        { x: new Date(2024, 0, 1), y: 232.52 },
-        { x: new Date(2024, 0, 2), y: 232 },
-        { x: new Date(2024, 0, 3), y: 230 },
-        { x: new Date(2024, 0, 4), y: 231 },
-        { x: new Date(2024, 0, 5), y: 235 },
-        { x: new Date(2024, 0, 6), y: 230 },
-        { x: new Date(2024, 0, 7), y: 231 },
-        { x: new Date(2024, 0, 8), y: 230 },
-        { x: new Date(2024, 0, 9), y: 229 },
-        { x: new Date(2024, 0, 10), y: 232 },
-        { x: new Date(2024, 0, 11), y: 230 },
-        { x: new Date(2024, 0, 12), y: 231 },
-      ],
-      click: function (e) {
-        // TODO: changing value by dragging the dataPoint
-        console.log(
-          e.dataSeries.type + " x:" + e.dataPoint.x + ", y: " + e.dataPoint.y
-        );
-      },
-    },
-  ],
-};
-
-let CanvasJS = CanvasJSReact.CanvasJS;
-let CanvasJSChart = CanvasJSReact.CanvasJSChart;
+const initialState = [
+  { x: new Date(2024, 0, 1), y: 232.52 },
+  { x: new Date(2024, 0, 2), y: 232 },
+  { x: new Date(2024, 0, 3), y: 230 },
+  { x: new Date(2024, 0, 4), y: 231 },
+  { x: new Date(2024, 0, 5), y: 235 },
+  { x: new Date(2024, 0, 6), y: 230 },
+  { x: new Date(2024, 0, 7), y: 231 },
+  { x: new Date(2024, 0, 8), y: 230 },
+  { x: new Date(2024, 0, 9), y: 229 },
+  { x: new Date(2024, 0, 10), y: 232 },
+  { x: new Date(2024, 0, 11), y: 230 },
+  { x: new Date(2024, 0, 12), y: 231 },
+];
 
 function App() {
+  const [state, setState] = useState(initialState);
+  const options = {
+    animationEnabled: true,
+    title: {
+      text: "Weight Tracker",
+    },
+    axisX: {
+      valueFormatString: "M/D",
+    },
+    axisY: {
+      title: "Pounds",
+      prefix: "",
+    },
+    data: [
+      {
+        yValueFormatString: "###.##",
+        xValueFormatString: "M/D",
+        type: "spline",
+        dataPoints: state,
+        click: function (e) {
+          // TODO: changing value by dragging the dataPoint
+          console.log(
+            e.dataSeries.type + " x:" + e.dataPoint.x + ", y: " + e.dataPoint.y
+          );
+        },
+        mouseover: function (e) {
+          e.dataPoint.y = 235;
+          setState(
+            state.map((dataPoint) => {
+              if (dataPoint.x === e.dataPoint.x) {
+                const updatedDataPoint = e.dataPoint;
+                updatedDataPoint.y = 235;
+                return updatedDataPoint;
+              }
+              return dataPoint;
+            })
+          );
+        },
+      },
+    ],
+  };
+
+  let CanvasJS = CanvasJSReact.CanvasJS;
+  let CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
   return (
     <div className="App">
       <CanvasJSChart
