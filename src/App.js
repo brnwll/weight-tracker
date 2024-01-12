c207071cd26d6af66e6e82b59acdc0b232b9ea13
Brian Howell <brianhowell@pm.me>
2024-01-12 18:02:54 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
 import React, { useState } from "react";
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+} from "recharts";
 import "./App.css";
 
 const initialState = [
-  { name: "1/1/24", lbs: 400 },
-  { name: "1/2/24", lbs: 200 },
-  { name: "1/4/24", lbs: 250 },
+  { x: "", y: null },
+  { x: "1/1/24", y: 235 },
+  { x: "1/2/24", y: 232 },
+  { x: "1/3/24", y: 229 },
+  { x: "1/4/24", y: 228 },
+  { x: "1/5/24", y: 229 },
+  { x: "1/6/24", y: 226 },
+  { x: "1/7/24", y: 228 },
+  { x: "1/8/24", y: 225 },
 ];
 
 function App() {
   const [state, setState] = useState(initialState);
 
-  const handleClick = (e) => {
-    
-  }
+  const handleClick = (e) => {};
 
   return (
     <div className="App">
-      <LineChart
-        width={600}
-        height={300}
-        data={state}
-        onClick={(e) => console.log(e)}
-      >
-        <Line type="monotone" dataKey="lbs" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="name" />
-        <YAxis />
-      </LineChart>
+      <div id="y-axis">
+        <ResponsiveContainer width={80} height="100%">
+          <LineChart className="custom-y-axis" data={state}>
+            <Line className="hide" dataKey="y" />
+            <XAxis className="hide" dataKey="x" />
+            <YAxis domain={[170, 240]} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+      <div id="yo-chart">
+        <ResponsiveContainer width={1000} height="100%">
+          <LineChart
+            className="custom-y-axis"
+            data={state}
+            onClick={(e) => {
+              console.log("onClick", e);
+            }}
+          >
+            <Line
+              type="monotone"
+              dataKey="y"
+              stroke="#8884d8"
+              strokeWidth={5}
+              activeDot={{ stroke: "red", strokeWidth: 2, r: 10 }}
+              onMouseDown={(e) => {
+                console.log("onMouseDown", e);
+              }}
+            />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="x" />
+            <YAxis className="hide" domain={[170, 240]} />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
